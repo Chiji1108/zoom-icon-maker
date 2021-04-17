a03e672ef19519890e1e23858aab9f3727738ff6
Chiji1108 <chiji@keio.jp>
2021-04-17 22:54:00 +0900

    fix for chromatic

--- src/lib/Button.tsx
+++ src/lib/Button.tsx
@@ -6,7 +6,7 @@ export default function Button({ ...props }: ButtonProps) {
   return (
     <button
       type="button"
-      className="py-2 px-4 bg-green-500 text-white font-semibold rounded-lg shadow-md disabled:shadow-none hover:bg-green-700 focus:outline-none disabled:opacity-50 disabled:hover:bg-blue-500 disabled:hover:cursor-not-allowed"
+      className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md disabled:shadow-none hover:bg-blue-700 focus:outline-none disabled:opacity-50 disabled:hover:bg-blue-500 disabled:hover:cursor-not-allowed"
       {...props}
     />
   );
