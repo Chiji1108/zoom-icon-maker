39a627a9732f7adc125a8f297e595fdf24d066f2
Chiji1108 <chiji@keio.jp>
2021-04-24 12:48:09 +0900

    add variant

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -8,6 +8,16 @@ export default {
 
 export const Primary: Story<ButtonProps> = () => <Button>Primary</Button>;
 
-export const Disabled: Story<ButtonProps> = () => (
+export const DisabledPrimary: Story<ButtonProps> = () => (
   <Button disabled>Disabled</Button>
+);
+
+export const Secondary: Story<ButtonProps> = () => (
+  <Button variant="secondary">Secondary</Button>
+);
+
+export const DisabledSecondary: Story<ButtonProps> = () => (
+  <Button disabled variant="secondary">
+    Disabled
+  </Button>
 );
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,23 @@
 import { ComponentPropsWithoutRef } from "react";
+import cn from "classnames";
 
-export interface ButtonProps extends ComponentPropsWithoutRef<"button"> {}
+export interface ButtonProps extends ComponentPropsWithoutRef<"button"> {
+  variant?: "primary" | "secondary";
+}
 
-// TODO: loading
-export default function Button({ ...props }: ButtonProps) {
+// TODO: improve style
+export default function Button({ variant = "primary", ...props }: ButtonProps) {
   return (
     <button
       type="button"
-      className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md disabled:shadow-none hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50 disabled:hover:bg-blue-500 disabled:hover:cursor-not-allowed transition-all"
+      className={cn(
+        "py-2 px-4 font-semibold rounded-lg disabled:shadow-none focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50 disabled:hover:cursor-not-allowed transition-all",
+        {
+          "shadow-md  bg-blue-500 text-white hover:bg-blue-700 disabled:hover:bg-blue-500 ":
+            variant === "primary",
+          "text-blue-500": variant === "secondary",
+        }
+      )}
       {...props}
     />
   );
