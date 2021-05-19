76bb94cc8ec33c1b05ee4980fa34b05c243b0b97
Chiji1108 <chiji@keio.jp>
2021-05-19 20:15:11 +0900

    🚧 disabled

--- src/components/Form/ZoomIconFormSlice.ts
+++ src/components/Form/ZoomIconFormSlice.ts
@@ -1,32 +1,32 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+// import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-interface ZoomIconFormState {
-  generator: {
-    status: "loading" | "idle";
-    error: Error;
-    artifact: string;
-  };
-  setting: {
-    useBio: boolean;
-    font: string;
-    icon: string;
-  };
-}
+// interface ZoomIconFormState {
+//   generator: {
+//     status: "loading" | "idle";
+//     error: Error;
+//     artifact: string;
+//   };
+//   setting: {
+//     useBio: boolean;
+//     font: string;
+//     icon: string;
+//   };
+// }
 
-const initialState: ZoomIconFormState = {
-  value: {
-    src: null,
-    name: null,
-    bio: null,
-  },
-  generator: {
-    status: Status.WAITING,
-    error: null,
-    artifact: null,
-  },
-  setting: {
-    useBio: true,
-    font: "Noto Sans JP",
-    icon: null,
-  },
-};
+// const initialState: ZoomIconFormState = {
+//   value: {
+//     src: null,
+//     name: null,
+//     bio: null,
+//   },
+//   generator: {
+//     status: Status.WAITING,
+//     error: null,
+//     artifact: null,
+//   },
+//   setting: {
+//     useBio: true,
+//     font: "Noto Sans JP",
+//     icon: null,
+//   },
+// };
