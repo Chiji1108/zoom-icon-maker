0aa4280f9b176e9029b56acbab23f0ad3907b394
Chiji1108 <chiji@keio.jp>
2021-05-19 20:22:19 +0900

    🐛 small fix

--- src/components/SettingInput/SettingInput.tsx
+++ src/components/SettingInput/SettingInput.tsx
@@ -1,3 +1,14 @@
+export interface Setting {
+  font: null | string;
+  enabledBio: boolean;
+  bioIcon: null | "Twitter" | "Instagram";
+}
+
+type SettingInputProps = {
+  value: Setting;
+  onChange: (nextValue: Setting) => void;
+};
+
 const SettingInput = () => {};
 
 const SettingContent = () => {};
