c8a6a38e4bb162af05a318a6efb7a38fd5950fb7
Chiji1108 <chiji@keio.jp>
2021-05-30 20:19:01 +0900

    ✨ add fontWeight

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,9 +1,18 @@
 import { ChakraProvider } from "@chakra-ui/react";
 
 import "@fontsource/material-icons";
-import "@fontsource/noto-sans-jp";
+import "@fontsource/noto-sans-jp/100.css";
+import "@fontsource/noto-sans-jp/300.css";
+import "@fontsource/noto-sans-jp/400.css";
+import "@fontsource/noto-sans-jp/500.css";
+import "@fontsource/noto-sans-jp/700.css";
+import "@fontsource/noto-sans-jp/900.css";
 import "@fontsource/dotgothic16";
-import "@fontsource/shippori-mincho";
+import "@fontsource/shippori-mincho/400.css";
+import "@fontsource/shippori-mincho/500.css";
+import "@fontsource/shippori-mincho/600.css";
+import "@fontsource/shippori-mincho/700.css";
+import "@fontsource/shippori-mincho/800.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
