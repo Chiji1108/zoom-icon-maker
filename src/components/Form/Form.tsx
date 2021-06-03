c0fd2392a7e9b8376a977d411ae8f7ba4aa96084
Chiji1108 <chiji@keio.jp>
2021-06-04 00:01:39 +0900

    ✨ update Form Component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -13,20 +13,41 @@ import {
   useEditableControls,
   Flex,
   HStack,
+  Center,
+  Stack,
+  Fade,
 } from "@chakra-ui/react";
 import { generate } from "../../lib/formUtils";
+import immer from "immer";
+import { useImmer } from "use-immer";
+import { Setting, SettingInput } from "../SettingInput";
+import icons from "../../lib/icons";
 
 export interface FormProps {}
 
 const Form = ({}: FormProps) => {
-  //TODO: initial value とか ボタンとかちゃんとする
   const [src, setSrc] = useState<string>();
-  const [name, setName] = useState("千々岩");
-  const [bio, setBio] = useState("Chijidosu");
-  const [setting, setSetting] = useState({
-    font: "Shippori Mincho",
-    enabledBio: true,
-    bioIcon: "Twitter",
+  const [bio, updateBio] = useImmer<Setting>({
+    text: "Chijidosu",
+    setting: {
+      font: {
+        family: "Noto Sans JP",
+        weight: "400",
+      },
+      icon: "none",
+      isHidden: false,
+    },
+  });
+  const [name, updateName] = useImmer<Setting>({
+    text: "千々岩",
+    setting: {
+      font: {
+        family: "Noto Sans JP",
+        weight: "400",
+      },
+      icon: "none",
+      isHidden: false,
+    },
   });
 
   const [result, setResult] = useState<string>();
@@ -34,50 +55,109 @@ const Form = ({}: FormProps) => {
   //TODO: add EditableControls
   return (
     <>
-      <Box p="4" bg="rgb(36,36,36)" maxW="sm">
+      <Box p="4" bg="rgb(36,36,36)" maxW="sm" pos="relative">
+        <Box pos="absolute" top="0" right="0">
+          <Fade in={bio.setting.isHidden} unmountOnExit>
+            <IconButton
+              borderRadius="full"
+              aria-label="add bio"
+              icon={<span className="material-icons">add</span>}
+              onClick={() =>
+                updateBio((draft) => {
+                  draft.setting.isHidden = false;
+                })
+              }
+              variant="ghost"
+              colorScheme="whiteAlpha"
+            />
+          </Fade>
+        </Box>
+
         <VStack spacing="4">
           <Box>
-            <AvatarInput value={src} onChange={setSrc} />
+            <AvatarInput
+              boxSize={bio.setting.isHidden ? 140 : 120}
+              value={src}
+              onChange={setSrc}
+            />
           </Box>
-          <Flex direction="column" alignItems="center">
-            <Editable
-              value={name}
-              onChange={(nextValue) => setName(nextValue)}
-              placeholder="名前を入力"
-            >
-              <Box
-                textColor="white"
-                fontFamily={setting.font}
-                textAlign="center"
-                fontSize="3xl"
+          <Flex
+            direction="column"
+            alignItems="center"
+            my={bio.setting.isHidden ? "2" : undefined}
+          >
+            <Box pos="relative">
+              <Editable
+                value={name.text}
+                onChange={(nextValue) =>
+                  updateName((draft) => {
+                    draft.text = nextValue;
+                  })
+                }
+                placeholder="名前を入力"
               >
-                <EditablePreview />
-                <EditableInput />
-              </Box>
-
-              {/* <EditableControls /> */}
-            </Editable>
-            <Editable
-              value={bio}
-              onChange={(nextValue) => setBio(nextValue)}
-              placeholder="所属/役職/連絡先"
-            >
-              <Flex fontSize="xs" alignItems="center">
-                <Box textColor="#1DA1F2" mr={1}>
-                  {/* <SiTwitter /> */}
-                </Box>
                 <Box
+                  verticalAlign={bio.setting.isHidden ? "super" : "text-bottom"}
+                  fontFamily={name.setting.font.family}
+                  fontWeight={name.setting.font.weight}
                   textColor="white"
-                  fontFamily={setting.font}
                   textAlign="center"
+                  fontSize={bio.setting.isHidden ? "4xl" : "3xl"}
                 >
                   <EditablePreview />
                   <EditableInput />
                 </Box>
-              </Flex>
 
-              {/* <EditableControls /> */}
-            </Editable>
+                {/* <EditableControls /> */}
+              </Editable>
+              <Center pos="absolute" right="-44px" top={0} bottom={0}>
+                <SettingInput
+                  value={name}
+                  onChange={updateName}
+                  advanced={false}
+                />
+              </Center>
+            </Box>
+            {bio.setting.isHidden || (
+              <Box pos="relative">
+                <Editable
+                  value={bio.text}
+                  onChange={(nextValue) =>
+                    updateBio((draft) => {
+                      draft.text = nextValue;
+                    })
+                  }
+                  placeholder="所属/役職/連絡先"
+                >
+                  <Flex fontSize="xs" alignItems="center">
+                    {bio.setting.icon !== "none" && (
+                      <Center width="12px" height="12px" m="1">
+                        {icons[bio.setting.icon].component}
+                      </Center>
+                    )}
+                    <Box
+                      textColor="white"
+                      verticalAlign="text-bottom"
+                      fontFamily={bio.setting.font.family}
+                      fontWeight={bio.setting.font.weight}
+                      textAlign="center"
+                    >
+                      <EditablePreview />
+                      <EditableInput />
+                    </Box>
+                  </Flex>
+
+                  {/* <EditableControls /> */}
+                </Editable>
+                <Center pos="absolute" right="-44px" top={0} bottom={0}>
+                  <SettingInput
+                    value={bio}
+                    onChange={updateBio}
+                    advanced={true}
+                  />
+                </Center>
+              </Box>
+            )}
           </Flex>
         </VStack>
       </Box>
@@ -89,7 +169,6 @@ const Form = ({}: FormProps) => {
               src,
               name,
               bio,
-              setting,
             })
           )
         }
