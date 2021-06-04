1137762421169bc6bf667272153c38581b376721
Chiji1108 <chiji@keio.jp>
2021-06-05 00:35:41 +0900

    ♻️ refactor code for fonts

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,18 +1,19 @@
 import { ChakraProvider } from "@chakra-ui/react";
 
 import "@fontsource/material-icons";
-import "@fontsource/noto-sans-jp/100.css";
-import "@fontsource/noto-sans-jp/300.css";
-import "@fontsource/noto-sans-jp/400.css";
-import "@fontsource/noto-sans-jp/500.css";
-import "@fontsource/noto-sans-jp/700.css";
-import "@fontsource/noto-sans-jp/900.css";
-import "@fontsource/dotgothic16";
-import "@fontsource/shippori-mincho/400.css";
-import "@fontsource/shippori-mincho/500.css";
-import "@fontsource/shippori-mincho/600.css";
-import "@fontsource/shippori-mincho/700.css";
-import "@fontsource/shippori-mincho/800.css";
+// import "@fontsource/noto-sans-jp/100.css";
+// import "@fontsource/noto-sans-jp/300.css";
+// import "@fontsource/noto-sans-jp/400.css";
+// import "@fontsource/noto-sans-jp/500.css";
+// import "@fontsource/noto-sans-jp/700.css";
+// import "@fontsource/noto-sans-jp/900.css";
+// import "@fontsource/dotgothic16";
+// import "@fontsource/shippori-mincho/400.css";
+// import "@fontsource/shippori-mincho/500.css";
+// import "@fontsource/shippori-mincho/600.css";
+// import "@fontsource/shippori-mincho/700.css";
+// import "@fontsource/shippori-mincho/800.css";
+// import "@fontsource/kosugi-maru";
 
 import * as nextImage from "next/image";
 
