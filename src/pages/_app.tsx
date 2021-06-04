f16b2903661fc945dcc6a518919fddfca53a0689
Chiji1108 <chiji@keio.jp>
2021-06-05 00:36:19 +0900

    ♻️ refactor code for fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,7 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 
-//TODO: lazyで読み込む
 import "@fontsource/material-icons";
-import "@fontsource/noto-sans-jp/100.css";
-import "@fontsource/noto-sans-jp/300.css";
-import "@fontsource/noto-sans-jp/400.css";
-import "@fontsource/noto-sans-jp/500.css";
-import "@fontsource/noto-sans-jp/700.css";
-import "@fontsource/noto-sans-jp/900.css";
-import "@fontsource/dotgothic16";
-import "@fontsource/shippori-mincho/400.css";
-import "@fontsource/shippori-mincho/500.css";
-import "@fontsource/shippori-mincho/600.css";
-import "@fontsource/shippori-mincho/700.css";
-import "@fontsource/shippori-mincho/800.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
