a36d7f83a50e949092f301f0fea0d861a1cdf4d9
Chiji1108 <chiji@keio.jp>
2021-06-05 00:32:58 +0900

    ✅ add jest

---
+++ jest.setup.ts
@@ -0,0 +1,20 @@
+// import { server } from "./src/mocks/server";
+
+import dotenv from "dotenv";
+import path from "path";
+
+// import "whatwg-fetch";
+// import "unfetch/polyfill";
+
+import "isomorphic-fetch";
+
+dotenv.config({
+  path: path.resolve(process.cwd(), ".env.local"),
+});
+
+// global.fetch = fetchPolyfill;
+// Object.assign(global.fetch, fetchPolyfill);
+
+// beforeAll(() => server.listen());
+// afterEach(() => server.resetHandlers());
+// afterAll(() => server.close());
