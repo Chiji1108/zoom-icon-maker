77f4e486bb07b8a170c618b4222e12caa274d687
Chiji1108 <chiji@keio.jp>
2021-06-05 00:37:17 +0900

    ✨ add twitter fetching

---
+++ src/__tests__/fetch.test.ts
@@ -0,0 +1,23 @@
+import { createMocks } from "node-mocks-http";
+import handleFetch from "../pages/api/twitter/[id]";
+
+describe("/api/twitter/[id]", () => {
+  test("returns a data", async () => {
+    const { req, res } = createMocks({
+      method: "GET",
+      query: {
+        id: "Chijidosu",
+      },
+    });
+
+    await handleFetch(req, res);
+
+    expect(res._getStatusCode()).toBe(200);
+    console.log(JSON.parse(res._getData()));
+    expect(JSON.parse(res._getData())).toEqual(
+      expect.objectContaining({
+        name: "千々岩",
+      })
+    );
+  });
+});
---
+++ src/pages/api/twitter/[id].ts
@@ -0,0 +1,48 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export type Data = {
+  name: string;
+  profile_image_url: string;
+  id: string | number;
+  username: string;
+};
+
+export type ErrorData = {
+  message: string;
+};
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse<Data | ErrorData | any>
+) => {
+  const { id } = req.query;
+  let response: Response | undefined = undefined;
+  try {
+    response = await fetch(
+      `https://api.twitter.com/2/users/by/username/${id}?${new URLSearchParams({
+        "user.fields": "profile_image_url",
+      })}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.API_TOKEN}`,
+        },
+      }
+    );
+    if (response.ok) {
+      const json = await response.json();
+
+      if (json.data) {
+        const data = json.data;
+        res.status(200).json({
+          ...data,
+          profile_image_url: data.profile_image_url.replace("_normal", ""),
+        });
+      } else if (json.errors) {
+        const errors = json.errors;
+        res.status(400).json(errors);
+      }
+    }
+  } catch (error) {
+    res.status(500).end();
+  }
+};
