40ffdc448b836447ac313a2f594aa23f71a4ac62
Chiji1108 <chiji@keio.jp>
2021-06-09 13:38:54 +0900

    ✅ add test

--- src/__tests__/fetch.test.ts
+++ src/__tests__/fetch.test.ts
@@ -1,5 +1,6 @@
 import { createMocks } from "node-mocks-http";
-import handleFetch from "../pages/api/twitter/[id]";
+import handleFetchTwitter from "../pages/api/twitter/[id]";
+import handleFetchUnsplash from "../pages/api/unsplash/search";
 
 describe("/api/twitter/[id]", () => {
   test("returns a data from Twitter", async () => {
@@ -10,7 +11,7 @@ describe("/api/twitter/[id]", () => {
       },
     });
 
-    await handleFetch(req, res);
+    await handleFetchTwitter(req, res);
 
     expect(res._getStatusCode()).toBe(200);
     // console.log(JSON.parse(res._getData()));
@@ -19,5 +20,35 @@ describe("/api/twitter/[id]", () => {
         name: "千々岩",
       })
     );
+  });
+});
+
+describe("/api/unsplash/search", () => {
+  test("returns a data from Unsplash", async () => {
+    const { req, res } = createMocks({
+      method: "GET",
+      query: {
+        query: "cat",
+        page: 1,
+      },
+    });
+
+    await handleFetchUnsplash(req, res);
+    // console.log(JSON.parse(res._getData()));
+    expect(res._getStatusCode()).toBe(200);
+  });
+
+  test("returns a error from Unsplash", async () => {
+    const { req, res } = createMocks({
+      method: "GET",
+      query: {
+        query: "cat",
+        // page: -500,
+      },
+    });
+
+    await handleFetchUnsplash(req, res);
+    console.log(JSON.parse(res._getData()));
+    expect(res._getStatusCode()).toBe(400);
   });
 });
