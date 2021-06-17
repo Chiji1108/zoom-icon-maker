dc1d1ec334952dd8730a45c21ae5b180fda99b58
Chiji1108 <chiji@keio.jp>
2021-06-17 13:16:08 +0900

    🚧 comment out unused code

--- src/lib/formUtils.ts
+++ src/lib/formUtils.ts
@@ -1,82 +1,84 @@
-//TODO: サイズ削除
-// import simpleIcons from "simple-icons";
+export {}
+// //TODO: サイズ削除
 
-import { createImage } from "./canvasUtils";
-import type { Setting } from "../components/SettingInput";
+// // import simpleIcons from "simple-icons";
 
-type Data = {
-  src?: string;
-  name: Setting;
-  bio: Setting;
-};
+// import { createImage } from "./canvasUtils";
+// import type { Setting } from "../components/SettingInput";
 
-// import { fabric } from "fabric";
+// type Data = {
+//   src?: string;
+//   name: Setting;
+//   bio: Setting;
+// };
 
-export const generate = async ({ src, name, bio }: Data) => {
-  //TODO: src がnullだったら
-  if (!src) {
-    throw new Error("src is not defined!");
-  }
+// // import { fabric } from "fabric";
 
-  const canvasEl = document.createElement("canvas");
-  canvasEl.width = 1024;
-  canvasEl.height = 1024;
-  const canvas = new fabric.StaticCanvas(canvasEl);
+// export const generate = async ({ src, name, bio }: Data) => {
+//   //TODO: src がnullだったら
+//   if (!src) {
+//     throw new Error("src is not defined!");
+//   }
 
-  const bg = new fabric.Rect({
-    left: 0,
-    top: 0,
-    width: canvas.getWidth(),
-    height: canvas.getHeight(),
-    fill: "rgb(36,36,36)",
-  });
-  canvas.add(bg);
+//   const canvasEl = document.createElement("canvas");
+//   canvasEl.width = 1024;
+//   canvasEl.height = 1024;
+//   const canvas = new fabric.StaticCanvas(canvasEl);
 
-  const imageEl = await createImage(src);
-  const image = new fabric.Image(imageEl, {
-    originX: "center",
-    originY: "center",
-    top: canvas.getHeight() / 2 - 130,
-    left: canvas.getWidth() / 2,
-    clipPath: new fabric.Circle({
-      radius: imageEl.width / 2,
-      originX: "center",
-      originY: "center",
-    }),
-  });
-  image.scaleToWidth(600);
-  image.scaleToHeight(600);
-  canvas.add(image);
+//   const bg = new fabric.Rect({
+//     left: 0,
+//     top: 0,
+//     width: canvas.getWidth(),
+//     height: canvas.getHeight(),
+//     fill: "rgb(36,36,36)",
+//   });
+//   canvas.add(bg);
 
-  const textName = new fabric.Text(name.text, {
-    fill: "#ffffff",
-    fontSize: 120,
-    fontFamily: name.setting.font.family,
-    fontWeight: name.setting.font.weight,
-    originX: "center",
-    originY: "center",
-    left: canvas.getWidth() / 2,
-    top: canvas.getHeight() / 2 + 300,
-  });
-  canvas.add(textName);
+//   const imageEl = await createImage(src);
+//   const image = new fabric.Image(imageEl, {
+//     originX: "center",
+//     originY: "center",
+//     top: canvas.getHeight() / 2 - 130,
+//     left: canvas.getWidth() / 2,
+//     clipPath: new fabric.Circle({
+//       radius: imageEl.width / 2,
+//       originX: "center",
+//       originY: "center",
+//     }),
+//   });
+//   image.scaleToWidth(600);
+//   image.scaleToHeight(600);
+//   canvas.add(image);
 
-  const textBio = new fabric.Text(bio.text, {
-    fill: "#ffffff",
-    fontSize: 40,
-    fontFamily: bio.setting.font.family,
-    fontWeight: bio.setting.font.weight,
-    originX: "center",
-    originY: "center",
-    left: canvas.getWidth() / 2,
-    top: canvas.getHeight() / 2 + 420,
-  });
-  canvas.add(textBio);
+//   const textName = new fabric.Text(name.text, {
+//     fill: "#ffffff",
+//     fontSize: 120,
+//     fontFamily: name.setting.font.family,
+//     fontWeight: name.setting.font.weight,
+//     originX: "center",
+//     originY: "center",
+//     left: canvas.getWidth() / 2,
+//     top: canvas.getHeight() / 2 + 300,
+//   });
+//   canvas.add(textName);
 
-  canvas.renderAll();
+//   const textBio = new fabric.Text(bio.text, {
+//     fill: "#ffffff",
+//     fontSize: 40,
+//     fontFamily: bio.setting.font.family,
+//     fontWeight: bio.setting.font.weight,
+//     originX: "center",
+//     originY: "center",
+//     left: canvas.getWidth() / 2,
+//     top: canvas.getHeight() / 2 + 420,
+//   });
+//   canvas.add(textBio);
 
-  return new Promise<string>((resolve) => {
-    canvasEl.toBlob((file) => {
-      resolve(URL.createObjectURL(file));
-    }, "image/png");
-  });
-};
+//   canvas.renderAll();
+
+//   return new Promise<string>((resolve) => {
+//     canvasEl.toBlob((file) => {
+//       resolve(URL.createObjectURL(file));
+//     }, "image/png");
+//   });
+// };
